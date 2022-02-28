c2bc2d5c13d3b3965b86443c6a3381b6d373dcd7
yusufemiral <100554355+yusufemiral@users.noreply.github.com>
2022-02-28 10:50:17 +0300

    Arrylist Şehir Uygulaması
    29_02_2022 Arrylist Şehir Uygulaması

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Arraylist_Sehirler_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList sehirler = new ArrayList();
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehirAdi.Text);
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sehirler.Count; i++)
+            {
+                lbListe.Items.Add(sehirler[i]);
+            }
+        }
+    }
+}
