40960062cc15f91c29da6e0e64ed6fa1db422e00
yusufemiral <100554355+yusufemiral@users.noreply.github.com>
2022-03-22 11:44:45 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -7,24 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Basit_veri_bağlama_uygulaması_2
+namespace Kompleks_veri_bağlama_uygulaması_form_tasarımı_2
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void btnBagla_Click(object sender, EventArgs e)
-        {
-             Ogrenciler ogrenci = new Ogrenciler();
-             ogrenci.Numara = 1111;
-             ogrenci.AdSoyad = "Mehmet";
-             ogrenci.Alan = "Bilişim Teknolojileri";
-             txtNumara.DataBindings.Add("Text", ogrenci, "Numara");
-             txtAdSoyad.DataBindings.Add("Text", ogrenci, "AdSoyad");
-             txtAlan.DataBindings.Add("Text", ogrenci, "Alan");
         }
     }
 }
