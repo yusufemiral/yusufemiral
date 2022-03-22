9603ec58a28e3a5f3902167359ae3c3e77327e37
yusufemiral <100554355+yusufemiral@users.noreply.github.com>
2022-03-22 11:55:40 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -7,13 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Kompleks_veri_bağlama_uygulaması_form_tasarımı_2
+namespace Giriş_maskeleme_uygulaması
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+                    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskTc.Mask = "00000000000";
+            maskTelefon.Mask = "(999) 000 00 00";
+            maskDTarihi.Mask = "00/00/0000";
+            maskKart.Mask = "0000000000000000";
+            maskIp.Mask = "###.###.###.###";
         }
     }
 }
---
+++ Veri doğrulama uygulaması form tasarımı.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Veri_doğrulama_uygulaması_form_tasarımı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            {
+            if (string.IsNullOrEmpty(txtAd.Text))
+ {
+           MessageBox.Show("Kullanıcı adını giriniz.", "Uyarı");
+ }
+          if(string.IsNullOrEmpty(txtSifre.Text))
+ {
+          MessageBox.Show("Şifreyi giriniz.", "Uyarı");
+ } 
+}
+        }
+    }
+}
